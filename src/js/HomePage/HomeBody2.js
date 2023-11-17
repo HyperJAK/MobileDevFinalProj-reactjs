40ec2f6b92f29a027d27a9f47ac0a732bbe2e6a7
HyperJAK <james.jakthesamething@gmail.com>
2023-11-17 21:47:59 +0200

    Added the image under the 3 buttons in Home

---
+++ src/js/HomePage/HomeBody2.js
@@ -0,0 +1,42 @@
+import imgSrc from "../../assets/4.jpg";
+import styled from 'styled-components';
+
+const Div = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: space-evenly;
+    `;
+
+
+const HomeImage = ()=> {
+    const imgSrc = require('../../assets/4.jpg');
+
+    const Img = styled.img`
+        width: 70%;
+        height: 500px;
+        margin: 0 auto;
+        display: block;
+        border-radius: 20px;
+      max-width: 1000px;
+    `;
+
+    return <Img src={imgSrc} alt="None"></Img>;
+}
+
+export default function HomeBody2({from, to, returnDate, departDate, setFrom, setTo, setDepartDate, setReturnDate, handleHomeQuickInputSearch}){
+
+
+
+    return(
+        <Div>
+
+            <HomeImage />
+        </Div>
+
+    );
+
+
+
+
+}
