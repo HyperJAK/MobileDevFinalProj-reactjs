73abf40915723c18675aba6552ad571376ab83c1
HyperJAK <james.jakthesamething@gmail.com>
2023-11-28 17:40:45 +0200

    Added Main div on all pages

--- src/js/FlightPage/Flight.js
+++ src/js/FlightPage/Flight.js
@@ -2,9 +2,15 @@ import React from 'react'
 import SearchFlight from './SearchFlight'
 import HomeBody1 from '../HomePage/HomeBody1'
 import FlightBody2 from './FlightBody2'
+import Spline from "@splinetool/react-spline";
+import {OverDesignText} from "../HomePage/OverDesignText";
 export default function Flight() {
   return (
     <>
+
+        <Spline scene="https://prod.spline.design/BOGpfRiHRnN9C-Wa/scene.splinecode" />
+        <OverDesignText />
+
         <SearchFlight />
         <HomeBody1 />
         <FlightBody2 />
--- src/js/HotelPage/Hotel.js
+++ src/js/HotelPage/Hotel.js
@@ -2,9 +2,15 @@ import React from 'react'
 import SearchHotel from './SearchHotel'
 import HomeBody1 from '../HomePage/HomeBody1'
 import HotelBody2 from './HotelBody2'
+import Spline from "@splinetool/react-spline";
+import {OverDesignText} from "../HomePage/OverDesignText";
 export default function Hotel() {
   return (
     <>
+
+        <Spline scene="https://prod.spline.design/9FI0ZU1nnSTP8Xwt/scene.splinecode" />
+        <OverDesignText />
+
         <SearchHotel />
         <HomeBody1 />
         <HotelBody2 />
